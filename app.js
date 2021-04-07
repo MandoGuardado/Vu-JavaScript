3cb24bf0daf7e6749f82bc54cdc1004e49fd46b5
Armando Guardado <armando.guardado.ag@gmail.com>
2021-04-06 21:34:09 -0700

    Updated app.js with first Vue method and use this keyword to access fields in the data object. In addition used concatenation to combine differrent values

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 new Vue({
-el: '#vue-app',
-data: {
-    FirstName: 'Armando',
+  el: '#vue-app',
+  data: {
+    firstName: 'Armando',
     lastName: "Guardado"
-}
+  },
+  methods:{
+    greet:function(type){
+
+      return 'Good ' + type + ' ' + this.firstName
+    }
+  }
+
 });
