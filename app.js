0888860407e1bbe149a286b60c2b93539cb02953
Armando Guardado <armando.guardado.ag@gmail.com>
2021-04-09 21:58:11 -0700

    Tutorial #8 - Two-way Data binding

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 new Vue({
   el: '#vue-app',
   data: {
-   age: 25,
-   y:0,
-   x:0
+      name: '',
+      age: ''
   },
-  methods:{
-    logName: function(){
-      console.log("Printed name");
-    },
-    logAge: function(){
-      console.log("Printed age");
-    }
-
+  methods: {
+      logName: function(){
+          console.log('you entered your name');
+      },
+      logAge: function(){
+          console.log('you entered your age');
+      }
   }
-
 });
