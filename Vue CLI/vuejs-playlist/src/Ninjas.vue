4bee2728a98d09e057937947085710cbe59f18a2
Armando Guardado <armando.guardado.ag@gmail.com>
2021-04-10 21:04:58 -0700

    Tutorial #20 Component CSS(scoped)- Allows you to apply css to specific component

--- Vue CLI/vuejs-playlist/src/Ninjas.vue
+++ Vue CLI/vuejs-playlist/src/Ninjas.vue
@@ -1,20 +1,29 @@
-
 <template>
-    <ul>
-        <!-- has to be capital V, was giving me an error using small case v. -->
-        <li v-for="ninja in ninjas">{{ninja }}</li>
-    </ul>
+    <div>
+        <h1>List of ninjas</h1>
+        <ul>
+            <li v-for="ninja in ninjas">{{ ninja }}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
     data () {
         return {
-          ninjas: ['Yoshi', 'Mario', 'Ryu', 'Armando']
+          ninjas: ['Yoshi', 'Mario', 'Ryu']
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
+h1{
+    color: green;
+}
+li{
+    list-style-type: none;
+    padding: 6px;
+}
 </style>
+
