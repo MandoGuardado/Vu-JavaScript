6a883f8b8f162837eb000ec6874767d0b6180e5e
Armando Guardado <armando.guardado.ag@gmail.com>
2021-04-15 15:59:43 -0700

    Tutorial #43 - Posting to Firebase

--- Vue CLI/vuejs-playlist/src/components/addBlog.vue
+++ Vue CLI/vuejs-playlist/src/components/addBlog.vue
@@ -58,11 +58,7 @@ export default {
     },
     methods: {
         post: function(){
-            this.$http.post('http://jsonplaceholder.typicode.com/posts', {
-                title: this.blog.title,
-                body: this.blog.content,
-                userId: 1
-            }).then(function(data){
+            this.$http.post('https://vue-blog-project-b79db-default-rtdb.firebaseio.com/posts.json', this.blog).then(function(data){
                 console.log(data);
                 this.submitted = true;
             });
